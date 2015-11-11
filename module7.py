a5f551a26573fe4332e09472ab1635fb585e5aa5
dev-git <dev-git@users.noreply.github.com>
2015-11-11 21:40:10 +1300

    Create module7.py

---
+++ module7.py
@@ -0,0 +1,57 @@
+##############################################
+# File Name: module7.py
+# Version: 1.0
+# Team No.: 27
+# Team Name:
+# Date: 11 Nov 15
+##############################################
+
+import RPi.GPIO as GPIO
+import time
+
+print 'Programming the PiBot...'
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+
+def forward(interval):
+      print 'forward'
+      GPIO.output(11, True)
+      GPIO.output(13, True)
+      time.sleep(interval)
+      GPIO.output(11, False)
+      GPIO.output(13, False)
+   
+def back(interval):
+      print 'back'
+      GPIO.output(7, True)
+      GPIO.output(15, True)
+      time.sleep(interval)
+      GPIO.output(7, False)
+      GPIO.output(15, False)
+
+def left(interval):
+      print 'left'
+      GPIO.output(13, True)
+      time.sleep(interval)
+      GPIO.output(13, False)
+      
+def right(interval):
+      print 'right'
+      GPIO.output(11, True)
+      time.sleep(interval)
+      GPIO.output(11, False)
+
+# Main instructions here      
+forward(2)
+right(2)
+left(2)
+back(2)
+
+GPIO.cleanup()
+   
+print "\nPiBot is going offline..."
