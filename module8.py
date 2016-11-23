a671d51c0ba2993a093aa6c18c4b10d2abd5a229
kps-user <niamh.p.bates@gmail.com>
2016-11-23 13:49:24 +1300

    Update module8.py

--- module8.py
+++ module8.py
@@ -7,7 +7,7 @@
 ##############################################
 
 import RPi.GPIO as GPIO
-import time
+import time 3
 import sys, tty, termios
 
 print '\nHi, I am PiBot, your very own learning robot.'
