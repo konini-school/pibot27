9b41dc493b88082ddc1fa6d6771b1e1937c9557b
kps-user <niamh.p.bates@gmail.com>
2016-11-30 13:50:59 +1300

    Update module7.py

--- module7.py
+++ module7.py
@@ -47,10 +47,10 @@ def right(interval):
       GPIO.output(11, False)
 
 # Main instructions here      
-forward(0)
-right(0)
-left(5)
-back(0)
+forward(2)
+right(2)
+left(2)
+back(2)
 
 GPIO.cleanup()
    
